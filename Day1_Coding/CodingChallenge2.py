66c72c5f20a5903a78f3aae98270b6ee4745ce90
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-04-25 04:04:23 -0400

    Day 1 Coding

---
+++ Day1_Coding/CodingChallenge2.py
@@ -0,0 +1,12 @@
+# multiply two number based on input user
+
+num1 = int(input("Enter num1 :\n"))
+num2 = int(input("Enter num2 :\n"))
+
+print("Multiplication of Number is :", num1 * num2)
+
+# another coding
+name = input("")
+print(len(name))
+
+# taking input from user and finding the length of string using len function we can find the length of string
