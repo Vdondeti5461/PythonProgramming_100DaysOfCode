c0df941e1f7dfd72f5a67135837d75656e2dc3f4
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-04-25 03:46:52 -0400

    Day 1 Coding

---
+++ Day1_Coding/Inputfunction.py
@@ -0,0 +1,5 @@
+# input function taking input from the user and print that input into console
+
+print("I am " + input("What is your name \"Avenger\" ?"))
+
+# application called Thonny check on internet how exactly Indentation works and how the code works
