6453f6d99105a93a94b1467aa7c392fc682bf5c9
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-04-25 02:54:44 -0400

    Day 1 Coding

---
+++ Day1_Coding/Basics.py
@@ -0,0 +1,16 @@
+# Learning concepts printing , commenting , Debugging , String manipulation and Variables
+
+
+# print function used to print the output in console -- Shows commenting always indicate what the program do
+print(" Hello Cats ! Welcome TO Python Programming")
+
+# commenting
+# debugging - when you get error or code throws error try to debug the code and fix the error .
+# Use stack overflow or internet with error and find and fix it
+
+# inside the quotation marks We have a sequence of characters - Strings in programming
+
+
+
+
+
