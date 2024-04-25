3baf19d13d053fa95736fbf91bee095b764b890a
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-04-25 04:51:09 -0400

    Day 1 Coding

---
+++ Day1_Coding/Project1.py
@@ -0,0 +1,16 @@
+
+# Band name generator using the concepts learned in Day1
+
+# printing the welcoming message
+print("Welcome to Road House Music")
+
+# taking input from user for name
+
+city = input("What City that you are grew up ?\n")
+
+Pet = input("What is the name of your Pet ?\n")  # used new line concept
+
+# printing the name  based on variables input taken from user using concatenation
+
+print('Your band name coule be ' + city + " " + Pet)
+
