ec68048f50fd2a391b6907260b51c497fd0444ad
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-04-27 07:17:10 -0400

    Day 1 Coding

---
+++ Day2_Coding/DataTypes.py
@@ -0,0 +1,22 @@
+# data types
+
+# string
+
+print("Hello"[0])  # indexing
+
+print("123" + "546")
+
+# strings concatenation
+
+#integer
+
+print(123+456)
+
+
+#floating number
+
+print(3.1413)
+
+#boolean
+
+print(True)
