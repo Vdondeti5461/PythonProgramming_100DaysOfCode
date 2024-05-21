56eb7d32e4a08acd12a7aab815daabbbb0a311dc
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-05-21 15:25:06 -0400

    Day 2 Coding

---
+++ Day2_Coding/f_string.py
@@ -0,0 +1,7 @@
+score = 0
+height = 1.8
+isWinning = True
+
+#using f string to use all variables in single line
+
+print(f"Your score is {score} , your height is {height} , you are winning is {isWinning}")
---
+++ Day2_Coding/roundFunction.py
@@ -0,0 +1,19 @@
+# round the function to two decimal point
+print(round(8 / 3, 2))
+print(type(4 / 2))
+
+# floor division - get integer directly without decimal
+print(8 // 3)
+
+result = 4 / 2
+
+result /= 2
+print(result)
+
+# Initial score
+Score = 0
+
+New_Score = print("Your Score is " + str(Score))
+
+# # team score goal add + 1 to score
+# New_Score += 1
