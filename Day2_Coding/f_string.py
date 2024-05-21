af1f3de0afea1462d1004ad0c9106dbf1862f97a
Vdondeti5461 <vdondeti.naidu@gmail.com>
2024-05-21 15:25:56 -0400

    Day 2 Coding

--- Day2_Coding/f_string.py
+++ Day2_Coding/f_string.py
@@ -4,4 +4,4 @@ isWinning = True
 
 #using f string to use all variables in single line
 
-print(f"Your score is {score} , your height is {height} , you are winning is {isWinning}")
+print(f"Your score is {score} , your height is {height} , you are winning is {isWinning}.")
